d439048bfbc798506bdd399d67a7312e480a1f92
beanbeanchris <christophercastroramirez@gmail.com>
2023-08-31 20:42:37 -0700

    Add start of software.

---
+++ spirit_guide/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import { RecentActors } from "@material-ui/icons";
+
+ReactDOM.render(<App />, document.getElementById('root'));
